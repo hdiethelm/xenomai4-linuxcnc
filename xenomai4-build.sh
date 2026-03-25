f989c24032fba90b1769c4ea974217daa1a89800
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-03-25 09:52:50 +0100

    Scripts and kconig

---
+++ xenomai4-build.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+#Xenomai4 Kernel-----------------------------------------
+#git clone https://gitlab.com/xenomai/xenomai4/linux-evl.git
+#git -C linux-evl checkout v6.12.67-evl2-rebase
+KERNEL_GIT_VERSION=53a3aa
+
+#git clone https://gitlab.com/xenomai/xenomai4/libevl.git
+#git -C libevl checkout r56
+LIBEVL_GIT_VERSION=11e6a1
+
+cd linux-evl
+cp ../kconfig-base-config-6.12.74+deb13+1-rt-amd64.txt .config #Base: debian trixie config-6.12.74+deb13+1-rt-amd64
+make oldconfig
+make -j16 deb-pkg LOCALVERSION=-xenomai4-$KERNEL_GIT_VERSION KDEB_PKGVERSION=$(make kernelversion)-1
+cd ..
+
+#Xenomai4 userspace tools-------------------------------------
+mkdir libevl-build
+cd libevl-build
+
+# Prepare the build directory
+meson setup -Dbuildtype=release -Dprefix=/opt/evl -Duapi=../linux-evl . ../libevl
+
+# Build it
+meson compile
+
+# Install the result
+ninja install
+
+#Cleanup-----------------------------------------------------
+mkdir deb
+mv *.deb deb
+rm *.changes *.buildinfo *.tar.gz *.dsc
+
+git -C linux-evl clean -fxd
+git -C linux-evl checkout -- .
+
+git add \
+  deb/linux-headers-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-1_amd64.deb \
+  deb/linux-image-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-1_amd64.deb
+
+git clean -f deb/
---
+++ xenomai4-prepare.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+#Xenomai4 kernel-----------------------------------------
+sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev dwarves git fakeroot rsync sbsigntool kernel-wedge
+
+#Xenomai4 libevl-------------------------------------
+sudo apt install libltdl-dev gdb devscripts meson ninja-build
+
