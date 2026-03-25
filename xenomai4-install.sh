d24bf4f55355559f0ca92b2cece52f773da25391
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-03-25 11:54:18 +0100

    Recommondatons from evl check
    Output:
    CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE=n
    CONFIG_ACPI_PROCESSOR_IDLE=y #Not yet solved
    CONFIG_DEBUG_LIST=y
    CONFIG_FTRACE=y
    CONFIG_COMPACTION=y

--- xenomai4-install.sh
+++ xenomai4-install.sh
@@ -4,8 +4,8 @@ XENOMAI_GIT_VERSION=73ad3b
 
 #Xenomai4 Kernel-----------------------------------------
 sudo dpkg -i \
-  deb/linux-headers-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-1_amd64.deb \
-  deb/linux-image-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-1_amd64.deb
+  deb/linux-headers-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-2_amd64.deb \
+  deb/linux-image-6.12.67-xenomai4-${KERNEL_GIT_VERSION}_6.12.67-2_amd64.deb
 
 #Reboot, check for xenomai:
 # sudo dmesg | grep -i xenomai
