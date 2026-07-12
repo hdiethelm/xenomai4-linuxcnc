2f0c045e102dbd6370b636a6a09e8188b0bd6681
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-07-12 23:16:12 +0200

    Improve libevl debian package

--- libevl-debian/README.source.ex
+++
@@ -1,9 +0,0 @@
-libevl for Debian
-----------------
-
-<This file describes information about the source package, see Debian policy
-manual section 4.14. You WILL either need to modify or delete this file.>
-
-
-
- -- Hannes Diethelm <hannes.diethelm@gmail.com>  Wed, 25 Mar 2026 12:22:44 +0100
--- libevl-debian/libevl.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libevl package.
-#
-0 4	* * *	root	[ -x /usr/bin/libevl_maintenance ] && /usr/bin/libevl_maintenance
