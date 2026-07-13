9810f5ea03e02861415f2b37ea03c229ee351dd4
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-07-13 14:46:49 +0200

    Increase version / fix versioning

--- xenomai4-build.sh
+++ xenomai4-build.sh
@@ -37,7 +37,7 @@ cd libevl
 #DEBEMAIL="hannes.diethelm@gmail.com" DEBFULLNAME="Hannes Diethelm" dh_make --createorig -p libevl_56
 #dh_auto_configure --buildsystem=meson -- -Duapi=$(pwd)/../linux-evl/usr/include
 cp -r ../libevl-debian/ debian
-DEBEMAIL="hannes.diethelm@gmail.com" DEBFULLNAME="Hannes Diethelm" dch -v ${LIBEVL_VERSION} "Update r${LIBEVL_VERSION}"
+DEBEMAIL="hannes.diethelm@gmail.com" DEBFULLNAME="Hannes Diethelm" dch -v ${LIBEVL_PACKAGE_VERSION} "Update r${LIBEVL_VERSION}"
 dpkg-buildpackage -b -uc
 cd ..
 
@@ -50,7 +50,8 @@ git -C linux-evl clean -fxd
 git -C libevl clean -fxd
 
 git add \
-  deb/libevl_${LIBEVL_VERSION}_amd64.deb
+  deb/libevl_${LIBEVL_PACKAGE_VERSION}_amd64.deb \
+  deb/libevl-test_${LIBEVL_PACKAGE_VERSION}_amd64.deb
 
 git add \
   deb/linux-headers-${KERNEL_PACKAGE_VERSION}_amd64.deb \
