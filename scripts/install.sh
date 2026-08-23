0227b2e016475b6a88f8f433a7fdc15e7205a24f
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-08-23 17:07:48 +0200

    Improve scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,10 +6,10 @@ set -e
 
 if [ "$BUILD_TYPE" = "kernel" -o "$BUILD_TYPE" = "kernel_rt" ]; then
     #Xenomai4 Kernel-----------------------------------------
-    sudo dpkg -i \
-      pkg-kernel/linux-headers-${KERNEL_PACKAGE_VERSION}_amd64.deb \
-      pkg-kernel/linux-image-${KERNEL_PACKAGE_VERSION}_amd64.deb \
-      pkg-kernel/linux-libc-evl-dev_${KERNEL_VERSION}-${KERNEL_VERSION_STUFFIX}_amd64.deb
+    sudo apt install --reinstall \
+      ./pkg-kernel/linux-headers-${KERNEL_PACKAGE_VERSION}_amd64.deb \
+      ./pkg-kernel/linux-image-${KERNEL_PACKAGE_VERSION}_amd64.deb \
+      ./pkg-kernel/linux-libc-evl-dev_${KERNEL_VERSION}-${KERNEL_VERSION_STUFFIX}_amd64.deb
 
     #Reboot, check for xenomai:
     # sudo dmesg | grep -i xenomai
@@ -17,15 +17,15 @@ fi
 
 if [ "$BUILD_TYPE" = "lib" ]; then
     #Xenomai4 userspace tools-------------------------------------
-    sudo dpkg -i \
-      pkg-libevl/libevl_${LIBEVL_PACKAGE_VERSION}_amd64.deb \
-      pkg-libevl/libevl-bin_${LIBEVL_PACKAGE_VERSION}_amd64.deb \
-      pkg-libevl/libevl-dev_${LIBEVL_PACKAGE_VERSION}_amd64.deb
+    sudo apt install --reinstall \
+      ./pkg-libevl/libevl_${LIBEVL_PACKAGE_VERSION}_amd64.deb \
+      ./pkg-libevl/libevl-bin_${LIBEVL_PACKAGE_VERSION}_amd64.deb \
+      ./pkg-libevl/libevl-dev_${LIBEVL_PACKAGE_VERSION}_amd64.deb
 fi
 
 if [ "$BUILD_TYPE" = "linuxcnc" ]; then
     #LinuxCNC-------------------------------------
-    sudo dpkg -i \
-      pkg-linuxcnc/linuxcnc-uspace_${LINUXCNC_PKG_VERSION}_amd64.deb \
-      pkg-linuxcnc/linuxcnc-doc-en_${LINUXCNC_PKG_VERSION}_all.deb
+    sudo apt install --reinstall \
+      ./pkg-linuxcnc/linuxcnc-uspace_${LINUXCNC_PKG_VERSION}_amd64.deb \
+      ./pkg-linuxcnc/linuxcnc-doc-en_${LINUXCNC_PKG_VERSION}_all.deb
 fi
